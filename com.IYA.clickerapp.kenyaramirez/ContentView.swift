0e1067f7dead22196ace2bd5bd1aadbf92f3c2ad
kenyaramirez <kenyaramireztarazon@gmail.com>
2025-09-04 17:15:38 -0700

    Initial Commit

---
+++ com.IYA.clickerapp.kenyaramirez/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  com.IYA.clickerapp.kenyaramirez
+//
+//  Created by Kenya Ramirez on 9/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ com.IYA.clickerapp.kenyaramirez/com_IYA_clickerapp_kenyaramirezApp.swift
@@ -0,0 +1,17 @@
+//
+//  com_IYA_clickerapp_kenyaramirezApp.swift
+//  com.IYA.clickerapp.kenyaramirez
+//
+//  Created by Kenya Ramirez on 9/4/25.
+//
+
+import SwiftUI
+
+@main
+struct com_IYA_clickerapp_kenyaramirezApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
